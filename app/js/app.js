a31dce343082da4eb88925c2d2ed0c851e5c3c52
DevinClark <dclarkdesign@gmail.com>
2013-07-02 17:18:26 -0500

    Removed the extra view, route, and controller to simplify the template.

--- app/js/app.js
+++ app/js/app.js
@@ -5,13 +5,9 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 
 	.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 		$locationProvider.html5Mode(true).hashPrefix('!');
 		$routeProvider
-			.when('/view1', {
+			.when('/', {
 				templateUrl: 'views/_partial1.html', 
 				controller: 'MyCtrl1'
 			})
-			.when('/view2', {
-				templateUrl: 'views/_partial2.html', 
-				controller: 'MyCtrl2'
-			})
-			.otherwise({redirectTo: '/view1'});
+			.otherwise({redirectTo: '/'});
 	}]);
