4723aba876acbb9bb3f7329f2d21ce87fb369256
DevinClark <dclarkdesign@gmail.com>
2013-07-02 14:43:05 -0500

    Changed partials directory to a views directory. Partials should be prepended with an underscore within the views directory.

--- app/js/app.js
+++ app/js/app.js
@@ -5,11 +5,11 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 
 	.config(['$routeProvider', function($routeProvider) {
 		$routeProvider
 			.when('/view1', {
-				templateUrl: 'partials/partial1.html', 
+				templateUrl: 'views/_partial1.html', 
 				controller: 'MyCtrl1'
 			})
 			.when('/view2', {
-				templateUrl: 'partials/partial2.html', 
+				templateUrl: 'views/_partial2.html', 
 				controller: 'MyCtrl2'
 			})
 			.otherwise({redirectTo: '/view1'});
